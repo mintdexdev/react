be3f27000da0e09dfb124546a65d82e31be99b85
mintdexdev <dexdevpersona@gmail.com>
2025-06-18 22:49:12 +0530

    created custom react components

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,11 +1,13 @@
 import Star from "./Star"
 
 function App() {
+  // evaluate expresssion
+  const user = "star";
   return (
     <>
       <Star />
-      <h2>Stellar</h2>
-      <p>starfalldex</p>
+      <h2>frost</h2>
+      <p>starfalldex is {user}</p>
     </>
   )
 }
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,44 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+export function InsideApp() {
+  return (
+    <>
+      <h2>Star</h2>
+      <p>Inside Star</p>
+    </>
+  )
+}
+
+/* wont work */
+// const RactElement = {
+//   type: 'a',
+//   props: {
+// href: 'https://google.com',
+// target: '_blank'
+//   },
+//   children: 'visit google'
+// }
+
+// pass an element
+const secondElement = (
+  <a href="https://google.com" target='_blank'> visit google</a >
+)
+const user = "this is star"
+
+// create element from react  
+const thirdElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Click to google',
+  user
+);
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <InsideApp />
+  // <ReactElement />
+  // secondElement
+  // thirdElement
+  <App />
 )
